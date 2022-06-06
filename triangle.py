3c0d1f8903607f3c4444be0ea9dda9724daca1f4
Yosua Giats <47804570+yosuagrs@users.noreply.github.com>
2022-06-06 23:06:08 +0700

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,17 @@
+print('input angka : ',end ="")
+y = int(input())
+
+
+for x in range(1,y+1):
+    n = ""
+    for _ in range(y-x):
+        n+="-"    
+    for _ in range(x):
+        n+="$"
+    for _ in range(x-1):
+        n+="$"    
+    for _ in range(y-x):
+        n+="-"
+    print(n)
+        
+    
